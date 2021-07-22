d447565db1afdc26ca3b72db981eacf3fa40f893
liberatos278 <72798747+liberatos278@users.noreply.github.com>
2021-07-22 16:27:08 +0200

    feat: winner highlight

--- dist/public/main.js
+++ dist/public/main.js
@@ -6,6 +6,7 @@ class Game {
         this.actualSymbol = b;
         this.playgroundSpecs = c;
         this.timer = 0;
+        this.killer = false;
     }
     generate() {
         for (let i = 0; i < Math.pow(this.playgroundSpecs.size, 2); i++) {
@@ -22,15 +23,15 @@ class Game {
         }
     }
     restart(wipe) {
+        if (this.killer)
+            return;
         const playground = document.getElementsByClassName('playground')[0];
         playground.innerHTML = '';
+        this.stopTimer(true);
         this.generate();
-        this.startTimer(this.playgroundSpecs.timer, true);
         this.switchSymbol('global');
-        const scoreboard = document.getElementById('score');
-        if (wipe) {
-            scoreboard.innerHTML = '0:0';
-        }
+        if (wipe)
+            this.setScore(0, 0, true);
     }
     switchSymbol(type) {
         const actualTurn = document.getElementById('actualTurnIco');
@@ -53,31 +54,39 @@ class Game {
         };
         return symbol;
     }
-    startTimer(time, restart) {
+    startTimer(time, restart, color) {
         const timer = document.getElementById('time');
         const text = timer.firstChild;
         if (restart)
-            clearInterval(this.timer);
+            this.stopTimer(true);
         text.textContent = `${time}  `;
-        timer.style.color = 'white';
+        timer.style.color = !color ? 'white' : color;
         this.timer = window.setInterval(function () {
             let timerTime = Number(timer.textContent);
             timerTime--;
             text.textContent = `${timerTime}  `;
             if (timerTime < 6)
-                timer.style.color = '#d92e2e';
+                timer.style.color = !color ? '#d92e2e' : color;
             if (timerTime < 1) {
-                clearInterval(GameLogic.timer);
+                GameLogic.stopTimer(true);
                 GameLogic.switchSymbol('current');
                 GameLogic.startTimer(GameLogic.playgroundSpecs.timer, true);
             }
         }, 1000);
     }
+    stopTimer(restart) {
+        const timer = document.getElementById('time');
+        clearInterval(this.timer);
+        timer.style.color = 'white';
+        if (restart) {
+            timer.firstChild.textContent = '30  ';
+        }
+    }
     setSquare(id) {
         const square = document.getElementById(id);
         const squareLogo = square.children[0];
         const symbol = this.getSymbol();
-        if (square.dataset.type)
+        if (square.dataset.type || this.killer)
             return;
         squareLogo.setAttribute('class', '');
         squareLogo.classList.add('fa', `fa-${symbol.name}`, 'fa-3x');
@@ -87,104 +96,90 @@ class Game {
         this.switchSymbol('current');
         this.checkCombinations(id);
     }
+    reportWin(type) {
+        this.setScore(type == 'times' ? 1 : 0, type == 'circle-o' ? 1 : 0, false);
+        this.switchSymbol('current');
+        this.killer = true;
+        this.stopTimer(true);
+        this.startTimer(5, true, '#66f542');
+        setTimeout(() => {
+            GameLogic.killer = false;
+            GameLogic.restart(false);
+        }, 5000);
+    }
+    setScore(crosses, rounds, wipe) {
+        const scoreboard = document.getElementById('score');
+        const score = scoreboard.innerText.split(':');
+        let newScore = `${Number(score[0]) + crosses}:${Number(score[1]) + rounds}`;
+        if (wipe)
+            newScore = '0:0';
+        scoreboard.innerText = newScore;
+    }
     checkCombinations(lastSquare) {
         const x = Number(((Number(lastSquare) < 10 ? '0' : '') + lastSquare).charAt(0));
         const y = Number(((Number(lastSquare) < 10 ? '0' : '') + lastSquare).charAt(1));
-        this.horizontalCheck(x);
-        this.verticalCheck(y);
-        this.crosswiseLPCheck(x, y);
-        this.crosswisePLCheck(x, y);
         this.checkEmpty();
+        this.evaluator(x, y);
     }
-    reportWin(type) {
-        const scoreboard = document.getElementById('score');
-        const score = scoreboard.innerText.split(':');
-        let newScore;
-        if (type == 'times')
-            newScore = `${Number(score[0]) + 1}:${score[1]}`;
-        else
-            newScore = `${score[0]}:${Number(score[1]) + 1}`;
-        scoreboard.innerText = newScore;
-        this.switchSymbol('current');
-        this.restart(false);
-    }
-    horizontalCheck(row) {
-        let currentSymbol;
-        let k = { symbol: undefined, index: 0 };
-        for (let i = 0; i < 10; i++) {
-            const id = String(((row !== 0 ? row : '') + '') + (i + ''));
-            const squareData = document.getElementById(id);
-            const filledWith = squareData.dataset.type;
-            if (filledWith !== currentSymbol) {
-                currentSymbol = filledWith;
-                k.symbol = currentSymbol;
-                k.index = 0;
-            }
-            if (k.symbol)
-                k.index++;
-            if (k.index == this.winLimit) {
-                this.reportWin(currentSymbol);
+    highlightSquares(squares) {
+        for (let id of squares) {
+            const square = document.getElementById(id);
+            if (square) {
+                square.style.backgroundColor = '#d4c600';
             }
         }
     }
-    verticalCheck(column) {
-        let currentSymbol;
-        let k = { symbol: undefined, index: 0 };
-        for (let i = 0; i < 10; i++) {
-            const id = String(((i !== 0 ? i : '') + '') + (column + ''));
-            const squareData = document.getElementById(id);
-            const filledWith = squareData.dataset.type;
-            if (filledWith !== currentSymbol) {
-                currentSymbol = filledWith;
-                k.symbol = currentSymbol;
-                k.index = 0;
+    evaluator(row, column) {
+        var _a, _b;
+        let direction;
+        let squares = {
+            horizontal: [],
+            vertical: [],
+            diagonalPL: [],
+            diagonalLP: []
+        };
+        for (direction in squares) {
+            const array = squares[direction];
+            for (let i = -10; i < 10; i++) {
+                let id = 'toCalc';
+                switch (direction) {
+                    case 'horizontal':
+                        id = String(((row !== 0 ? row : '') + '') + (i + ''));
+                        break;
+                    case 'vertical':
+                        id = String(((i !== 0 ? i : '') + '') + (column + ''));
+                        break;
+                    case 'diagonalLP':
+                        id = String(row + i !== 0 ? row + i : '') + String(column + i);
+                        break;
+                    case 'diagonalPL':
+                        id = String(row + i !== 0 ? row + i : '') + String(column - i);
+                        break;
+                }
+                const square = document.getElementById(id);
+                if (!square)
+                    continue;
+                squares[direction].push(id);
             }
-            if (k.symbol)
-                k.index++;
-            if (k.index == this.winLimit) {
-                this.reportWin(currentSymbol);
-            }
-        }
-    }
-    crosswiseLPCheck(row, column) {
-        let currentSymbol;
-        let k = { symbol: undefined, index: 0 };
-        for (let i = -10; i < 10; i++) {
-            const id = String(row + i !== 0 ? row + i : '') + String(column + i);
-            const squareData = document.getElementById(String(id));
-            if (!squareData)
-                continue;
-            const filledWith = squareData.dataset.type;
-            if (filledWith !== currentSymbol) {
-                currentSymbol = filledWith;
-                k.symbol = currentSymbol;
-                k.index = 0;
-            }
-            if (k.symbol)
-                k.index++;
-            if (k.index == this.winLimit) {
-                this.reportWin(currentSymbol);
-            }
-        }
-    }
-    crosswisePLCheck(row, column) {
-        let currentSymbol;
-        let k = { symbol: undefined, index: 0 };
-        for (let i = -10; i < 10; i++) {
-            const id = String(row + i !== 0 ? row + i : '') + String(column - i);
-            const squareData = document.getElementById(String(id));
-            if (!squareData)
-                continue;
-            const filledWith = squareData.dataset.type;
-            if (filledWith !== currentSymbol) {
-                currentSymbol = filledWith;
-                k.symbol = currentSymbol;
-                k.index = 0;
-            }
-            if (k.symbol)
-                k.index++;
-            if (k.index == this.winLimit) {
-                this.reportWin(currentSymbol);
+            let currentSymbol;
+            let result = {
+                symbol: undefined,
+                index: []
+            };
+            for (let id of array) {
+                const square = document.getElementById(id);
+                const squareSymbol = square.dataset.type;
+                if (squareSymbol !== currentSymbol) {
+                    currentSymbol = squareSymbol;
+                    result.symbol = currentSymbol;
+                    result.index = [];
+                }
+                if (result.symbol)
+                    (_a = result.index) === null || _a === void 0 ? void 0 : _a.push(id);
+                if (((_b = result.index) === null || _b === void 0 ? void 0 : _b.length) == this.winLimit) {
+                    this.highlightSquares(result.index);
+                    this.reportWin(result.symbol);
+                }
             }
         }
     }
--- src/public/main.ts
+++ src/public/main.ts
@@ -4,10 +4,18 @@ interface playground {
     timer: number
 }
 
+interface evaluator {
+    horizontal: string[],
+    vertical: string[],
+    diagonalLP: string[],
+    diagonalPL: string[]
+}
+
 class Game {
     public startSymbol: boolean; // false = X; true = O
     public actualSymbol: boolean;
     public timer: number;
+    public killer: boolean;
     readonly playgroundSpecs: playground;
     readonly winLimit: number;
 
@@ -17,6 +25,7 @@ class Game {
         this.actualSymbol = b;
         this.playgroundSpecs = c;
         this.timer = 0;
+        this.killer = false;
     }
 
     generate(): void {
@@ -41,19 +50,16 @@ class Game {
 
     restart(wipe: boolean): void {
 
+        if (this.killer) return;
+
         const playground = document.getElementsByClassName('playground')[0] as HTMLDivElement;
         playground.innerHTML = '';
 
+        this.stopTimer(true);
         this.generate();
-        this.startTimer(this.playgroundSpecs.timer, true);
         this.switchSymbol('global');
 
-        const scoreboard = document.getElementById('score') as HTMLSpanElement;
-
-        if (wipe) {
-
-            scoreboard.innerHTML = '0:0';
-        }
+        if (wipe) this.setScore(0, 0, true);
     }
 
     switchSymbol(type: 'current' | 'global' | 'refreshonly'): void {
@@ -85,15 +91,15 @@ class Game {
         return symbol;
     }
 
-    startTimer(time: number, restart: boolean) {
+    startTimer(time: number, restart: boolean, color?: string) {
         const timer = document.getElementById('time')!;
         const text = timer.firstChild!;
 
-        if (restart) clearInterval(this.timer);
+        if (restart) this.stopTimer(true);
 
         text.textContent = `${time}  `;
-        timer.style.color = 'white';
-        
+        timer.style.color = !color ? 'white' : color;
+
         this.timer = window.setInterval(function () {
 
             let timerTime = Number(timer.textContent)
@@ -101,13 +107,26 @@ class Game {
 
             text.textContent = `${timerTime}  `;
 
-            if (timerTime < 6) timer.style.color = '#d92e2e';
+            if (timerTime < 6) timer.style.color = !color ? '#d92e2e' : color;
             if (timerTime < 1) {
-                clearInterval(GameLogic.timer);
+                GameLogic.stopTimer(true)
                 GameLogic.switchSymbol('current');
                 GameLogic.startTimer(GameLogic.playgroundSpecs.timer, true);
             }
         }, 1000);
+    }
+
+    stopTimer(restart: boolean) {
+
+        const timer = document.getElementById('time')!;
+
+        clearInterval(this.timer);
+        timer.style.color = 'white';
+
+        if (restart) {
+
+            timer.firstChild!.textContent = '30  ';
+        }
     }
 
     setSquare(id: string): void {
@@ -116,7 +135,7 @@ class Game {
         const squareLogo = square.children[0] as HTMLSpanElement;
         const symbol = this.getSymbol();
 
-        if (square.dataset.type) return;
+        if (square.dataset.type || this.killer) return;
 
         squareLogo.setAttribute('class', '');
         squareLogo.classList.add('fa', `fa-${symbol.name}`, 'fa-3x');
@@ -129,148 +148,125 @@ class Game {
         this.checkCombinations(id);
     }
 
+    reportWin(type: string | undefined): void {
+
+        this.setScore(type == 'times' ? 1 : 0, type == 'circle-o' ? 1 : 0, false);
+        this.switchSymbol('current');
+        this.killer = true;
+        
+        this.stopTimer(true);
+        this.startTimer(5, true, '#66f542');
+
+        setTimeout(() => {
+            GameLogic.killer = false;
+            GameLogic.restart(false);
+        }, 5000);
+    }
+
+    setScore(crosses: number, rounds: number, wipe: boolean): void {
+
+        const scoreboard = document.getElementById('score')!;
+        const score = scoreboard.innerText.split(':');
+        let newScore = `${Number(score[0]) + crosses}:${Number(score[1]) + rounds}`;
+
+        if (wipe) newScore = '0:0';
+        scoreboard.innerText = newScore;
+    }
+
     checkCombinations(lastSquare: string) {
 
         const x = Number(((Number(lastSquare) < 10 ? '0' : '') + lastSquare).charAt(0));
         const y = Number(((Number(lastSquare) < 10 ? '0' : '') + lastSquare).charAt(1));
 
-        this.horizontalCheck(x);
-        this.verticalCheck(y);
-        this.crosswiseLPCheck(x, y);
-        this.crosswisePLCheck(x, y);
+
         this.checkEmpty();
+        this.evaluator(x, y);
     }
 
-    reportWin(type: string | undefined): void {
-        
-        const scoreboard = document.getElementById('score')!;
-        const score = scoreboard.innerText.split(':');
-        let newScore: string;
+    highlightSquares(squares: string[]) {
 
-        if (type == 'times') newScore = `${Number(score[0]) + 1}:${score[1]}`; else newScore = `${score[0]}:${Number(score[1]) + 1}`;
-        scoreboard.innerText = newScore;
+        for (let id of squares) {
 
-        this.switchSymbol('current');
-        this.restart(false);
-    }
+            const square = document.getElementById(id);
+            if (square) {
 
-    horizontalCheck(row: number) {
-
-        let currentSymbol;
-        let k: {
-            symbol: string | undefined,
-            index: number | undefined
-        } = { symbol: undefined, index: 0 };
-
-        for (let i = 0; i < 10; i++) {
-            const id = String(((row !== 0 ? row : '') + '') + (i + ''));
-            const squareData = document.getElementById(id)!;
-
-            const filledWith = squareData.dataset.type;
-
-            if (filledWith !== currentSymbol) {
-
-                currentSymbol = filledWith;
-                k.symbol = currentSymbol;
-                k.index = 0;
-            }
-
-            if (k.symbol) k.index!++;
-            if (k.index == this.winLimit) {
-                this.reportWin(currentSymbol);
+                square.style.backgroundColor = '#d4c600';
             }
         }
     }
 
-    verticalCheck(column: number) {
+    evaluator(row: number, column: number) {
 
-        let currentSymbol;
-        let k: {
-            symbol: string | undefined,
-            index: number | undefined
-        } = { symbol: undefined, index: 0 };
+        let direction: string;
+        let squares: evaluator = {
+            horizontal: [],
+            vertical: [],
+            diagonalPL: [],
+            diagonalLP: []
+        }
 
-        for (let i = 0; i < 10; i++) {
-            const id = String(((i !== 0 ? i : '') + '') + (column + ''));
-            const squareData = document.getElementById(id)!;
+        for (direction in squares) {
 
-            const filledWith = squareData.dataset.type;
+            const array = (squares as any)[direction];
 
-            if (filledWith !== currentSymbol) {
+            for (let i = -10; i < 10; i++) {
 
-                currentSymbol = filledWith;
-                k.symbol = currentSymbol;
-                k.index = 0;
+                let id: string = 'toCalc';
+                
+                switch (direction) {
+                    case 'horizontal':
+                        id = String(((row !== 0 ? row : '') + '') + (i + ''));
+                        break;
+                    
+                    case 'vertical':
+                        id = String(((i !== 0 ? i : '') + '') + (column + ''));
+                        break;
+
+                    case 'diagonalLP':
+                        id = String(row + i !== 0 ? row + i : '') + String(column + i);
+                        break;
+
+                    case 'diagonalPL':
+                        id = String(row + i !== 0 ? row + i : '') + String(column - i);
+                        break;
+                }
+
+                const square = document.getElementById(id);
+                if (!square) continue;
+
+                (squares as any)[direction].push(id);
             }
 
-            if (k.symbol) k.index!++;
-            if (k.index == this.winLimit) {
-                this.reportWin(currentSymbol);
+
+            let currentSymbol;
+            let result: { symbol: string | undefined, index: string[] } = {
+                symbol: undefined,
+                index: []
+            }
+
+            for (let id of array) {
+                
+                const square = document.getElementById(id)!;
+
+                const squareSymbol = square.dataset.type;
+                if (squareSymbol !== currentSymbol) {
+
+                    currentSymbol = squareSymbol;
+                    result.symbol = currentSymbol;
+                    result.index = [];
+                }
+
+                if (result.symbol) result.index?.push(id);
+                if (result.index?.length == this.winLimit) {
+
+                    this.highlightSquares(result.index);
+                    this.reportWin(result.symbol);
+                }
             }
         }
     }
 
-    crosswiseLPCheck(row: number, column: number) {
-        
-        let currentSymbol;
-        let k: {
-            symbol: string | undefined,
-            index: number | undefined
-        } = { symbol: undefined, index: 0 };
-
-        for (let i = -10; i < 10; i++) {
-            const id = String(row + i !== 0 ? row + i : '') + String(column + i);
-            const squareData = document.getElementById(String(id))!;
-
-            if(!squareData) continue;
-
-            const filledWith = squareData.dataset.type;
-
-            if (filledWith !== currentSymbol) {
-
-                currentSymbol = filledWith;
-                k.symbol = currentSymbol;
-                k.index = 0;
-            }
-
-            if (k.symbol) k.index!++;
-            if (k.index == this.winLimit) {
-                this.reportWin(currentSymbol);
-            }
-        }
-    }
-
-    crosswisePLCheck(row: number, column: number) {
-        
-        let currentSymbol;
-        let k: {
-            symbol: string | undefined,
-            index: number | undefined
-        } = { symbol: undefined, index: 0 };
-
-        for (let i = -10; i < 10; i++) {
-            const id = String(row + i !== 0 ? row + i : '') + String(column - i);
-            const squareData = document.getElementById(String(id))!;
-
-            if(!squareData) continue;
-
-            const filledWith = squareData.dataset.type;
-
-            if (filledWith !== currentSymbol) {
-
-                currentSymbol = filledWith;
-                k.symbol = currentSymbol;
-                k.index = 0;
-            }
-
-            if (k.symbol) k.index!++;
-            if (k.index == this.winLimit) {
-                this.reportWin(currentSymbol);
-            }
-        }
-    }
-
-    checkEmpty() {
+    checkEmpty(): void {
 
         const emptySquares = [];
         for (let i = 0; i < 100; i++) {
